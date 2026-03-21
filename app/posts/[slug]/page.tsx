6f8e5da9763cd1b13f2ebb90447f3d165de7061e
akimasanishida <90087433+akimasanishida@users.noreply.github.com>
2026-03-21 10:08:54 +0900

    時刻チェック

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -15,12 +15,37 @@ export default async function Page(props: {
     notFound();
   }
 
+  const optionsLocaleString = {
+    timeZone: "Asia/Tokyo"
+  };
+
+  console.log("post.published_at", post.published_at);
+
+  const strPublishedAt = post.published_at
+    ? new Date(post.published_at).toLocaleString("ja-JP", {
+        timeZone: "Asia/Tokyo",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+    })
+    : null;
+  const strUpdatedAt = post.updated_at
+    ? new Date(post.updated_at).toLocaleDateString("ja-JP", {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+      })
+    : null;
   const contentHtml = await renderMarkdownToHTML(post.content || "");
 
   // TODO: This is just for development rendering. We will add more features later.
   return (
-    <div>
-      <h1>{post.title}</h1>
+    <div className="article">
+      <h1 className="text-4xl mb-2">{post.title}</h1>
+      <p className="text-gray-500 mb-4">公開日: {strPublishedAt}</p>
+      <p className="text-gray-500 mb-4">更新日: {strUpdatedAt}</p>
       <div dangerouslySetInnerHTML={{ __html: contentHtml }} />
     </div>
   );
