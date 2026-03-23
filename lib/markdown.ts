b651079b992560da610cf642de2dfc328dea84f5
akimasanishida <90087433+akimasanishida@users.noreply.github.com>
2026-03-23 14:12:56 +0900

    MD の画像をレンダできるようにした

--- lib/markdown.ts
+++ lib/markdown.ts
@@ -1,9 +1,11 @@
 import { unified } from "unified";
 import remarkParse from "remark-parse";
+import rehypeUnwrapImages from "rehype-unwrap-images";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
 import remarkEmoji from "remark-emoji";
 import remarkRehype from "remark-rehype";
+import rehypeRerwite from "rehype-rewrite";
 import rehypeRaw from "rehype-raw";
 import rehypeKatex from "rehype-katex";
 import rehypePrismPlus from "rehype-prism-plus";
@@ -11,13 +13,59 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeStringify from "rehype-stringify";
 import rehypeSlug from "rehype-slug";
 
-export async function renderMarkdownToHTML(markdownContent: string): Promise<string> {
+export async function renderMarkdownToHTML(
+  markdownContent: string,
+): Promise<string> {
   const processor = unified()
     .use(remarkParse) // Parse Markdown
+    .use(rehypeUnwrapImages) // Unwrap images <img> from paragraphs <p>
     .use(remarkGfm) // GitHub Flavored Markdown
     .use(remarkMath) // Math syntax (e.g., $E=mc^2$)
     .use(remarkEmoji) // Emoji (e.g., :tada:)
     .use(remarkRehype, { allowDangerousHtml: true }) // Convert to Rehype (HTML AST)
+    .use(rehypeRerwite, {
+      rewrite: (node, index, parent) => {
+        // Rewrite image URLs to be absolute paths
+        if (node.type === "element" && node.tagName === "img") {
+          const fullSrc = `${process.env.NEXT_PUBLIC_STORAGE_PUBLIC_URL}/${node.properties.src}`;
+          const title = node.properties.title
+            ? String(node.properties.title)
+            : undefined;
+          if (
+            parent &&
+            parent.type === "element" &&
+            parent.tagName !== "figure"
+          ) {
+            node.tagName = "figure";
+            node.children = [
+              {
+                type: "element",
+                tagName: "img",
+                properties: {
+                  ...node.properties,
+                  src: fullSrc,
+                  title: title,
+                },
+                children: [],
+              },
+            ];
+            if (title) {
+              node.children.push({
+                type: "element",
+                tagName: "figcaption",
+                properties: {},
+                children: [
+                  {
+                    type: "text",
+                    value: title,
+                  },
+                ],
+              });
+            }
+          }
+        }
+      },
+    })
     .use(rehypeRaw) // Allow raw HTML in Markdown
     .use(rehypeKatex) // Render math with KaTeX
     .use(rehypePrismPlus, { ignoreMissing: true, showLineNumbers: true }) // Code syntax highlighting
@@ -35,4 +83,4 @@ export async function renderMarkdownToHTML(markdownContent: string): Promise<str
   const contentHtml = result.toString();
 
   return contentHtml;
-};
+}
