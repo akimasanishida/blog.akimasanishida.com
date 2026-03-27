3224a94674ca46b2b59fa78144c3e59818e0c92a
akimasanishida <90087433+akimasanishida@users.noreply.github.com>
2026-03-27 12:11:06 +0900

    トップページにページネーションを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
-import { fetchPostsMetaData } from "@/lib/data";
+import { fetchPostsMetaData, fetchTotalPostsCount } from "@/lib/data";
 import PostsList from "@/components/PostsList";
+import { PaginationForPages } from "@/components/Pagination";
 
 export default async function Page() {
   const posts = await fetchPostsMetaData(0, 7, false, "desc");
+  const totalPostsCount = await fetchTotalPostsCount(false);
+  const totalPages = Math.ceil(totalPostsCount / 7);
 
   return (
-    <PostsList title="ブログ記事一覧" posts={posts} />
+    <>
+      <PostsList title="ブログ記事一覧" posts={posts} />
+      <div className="my-8 flex justify-center">
+        <PaginationForPages
+          currentPage={1}
+          numPages={totalPages}
+          basePath="/pages/"
+        />
+      </div>
+    </>
   );
 }
