84c398d4f04124c0a20ea574132d604e50937164
akimasanishida <90087433+akimasanishida@users.noreply.github.com>
2026-04-05 10:07:33 +0900

    日付をフォーマット

--- app/admin/columns.tsx
+++ app/admin/columns.tsx
@@ -2,6 +2,7 @@
 
 import { ColumnDef } from "@tanstack/react-table";
 import type { Post } from "@/types/posts";
+import { optionsLocaleString } from "@/lib/definitions";
 
 export const columns: ColumnDef<Post>[] = [
   {
@@ -11,10 +12,20 @@ export const columns: ColumnDef<Post>[] = [
   {
     accessorKey: "published_at",
     header: "公開日時",
+    cell: ({ row }) => {
+      const publishedAt =row.getValue("published_at");
+      if (!publishedAt) return "―";
+      return new Date(String(publishedAt)).toLocaleString("ja-JP", optionsLocaleString);
+    },
   },
   {
     accessorKey: "updated_at",
     header: "更新日時",
+    cell: ({ row }) => {
+      const updatedAt = row.getValue("updated_at");
+      if (!updatedAt) return "―";
+      return new Date(String(updatedAt)).toLocaleString("ja-JP", optionsLocaleString);
+    },
   },
   {
     accessorKey: "category",
@@ -23,5 +34,9 @@ export const columns: ColumnDef<Post>[] = [
   {
     accessorKey: "is_public",
     header: "公開/下書き",
-  }
-]
+    cell: ({ row }) => {
+      const isPublic = row.getValue("is_public");
+      return isPublic ? "公開" : "下書き";
+    },
+  },
+];
